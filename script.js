ec8fc3ff7184aca9a5b8cae3e397d2d44daf33bb
G-Vale <gabrielcrc.pj@gmail.com>
2022-08-10 15:25:43 -0300

    add menu e carteira
    So o basico

---
+++ script.js
@@ -0,0 +1,16 @@
+// Usando variavel para acompanhar se uma musica ja esta em uma lista de reproduçao
+// ou nao para um site de streaming de musica
+
+let song = "Don't stop me now"
+let included = true;
+
+console.log("New entry: " + song);
+if (included) {
+    console.log ("Already included!");
+}
+console.log ("Save and continue");
+
+
+// CARTEIRA ADICIONA OU REMOVER 
+
+const carteira = 42
