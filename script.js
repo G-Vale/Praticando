2ad1af0f0632feca2f51cba349a7ea99a9485e58
G-Vale <gabrielcrc.pj@gmail.com>
2022-08-15 16:18:46 -0300

    15/08 Atulização CSS,JS
    alteraçoes praticas no CSS
    Estudo ARRAYS e fUNÇOES JAVASCRIPT

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ console.log ("Save and continue");
 // CARTEIRA ADICIONA OU REMOVER 
 
 const carteira = 42
+
+// LISTA DE AMIGOS EM COMUM (ADD STRINGS A UMA VARIAVEL)
+
+const amigo1 = "Jean"
+const amigo2 = "Hector"
+const amigo3 = "Flavio"
+const amigo4 = "Ana"
+
+// Para melhor organizaçao usamos ARRAY (''[]'')
+
+const amigo = ["Jean", "Hector", "Flavio", "Ana"];
+
+// Para acessar um strins dentro do ARRAY
+
+amigo [0] = 2 ;   // Selecionou "JEAN"
+amigo [3] = 0 ;   // Selecionou "ANA"
+
+// ARRAY de Push e Popping, Adicionando e removendo valores com ARRAYS. "LISTA DE TAREFAS"
+
+amigo.push("Renan");   // Para Add um elemento no ARRY
+amigo.pop("Renan");    // Para Remover um elemento do ARRAY
+
+// Pode remover ou adicionar qualque coisa ate TRUE/FALSE
+
+const ordem = [false, false, true]
+ordem .push ();
+console.log (ordem);
+ordem .pop ()
+console.log (ordem)
+
+// Como contar os elementos na ARRAY
+
+const ruas = [2, 4, 7, 76,  354, 54, 3232, 278, 32, 0, 43, 231];
+console.log(ruas.length);
+    for (let i = 0; i < ruas.length; i++){
+        console.log(ruas[i])
+    }
+// Como usadon para contar uma ARRAY, usando LENGTH 
